112dbc71ff1cd2d81d0b3540a05b492f70c8f4b5
Michael Kaufman <mkaufman94@gmail.com>
2019-04-17 12:19:20 -0400

    feat: Pass attributes by counter object

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -35,9 +35,7 @@ class Counters extends Component {
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
-            value={counter.value}
-            selected={true}
-            id={counter.id}
+            counter={counter}
           />
         ))}
       </div>
